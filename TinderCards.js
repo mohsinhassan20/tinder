3acb4f8536226ab8b93232c724405c9fe8f4ca3b
mohsinhassan20 <mohsinbwp2018@gmail.com>
2022-03-08 01:08:33 +0500

    Update TinderCards.js

--- TinderCards.js
+++ TinderCards.js
@@ -1,9 +1,44 @@
-import React from 'react'
-import "./TinderCards.css"
+import { Directions, SwipeDown } from "@mui/icons-material";
+import React, { useState } from "react";
+import "./TinderCards.css";
+
 function TinderCards() {
+  const [people, setpeople] = useState([
+    {
+      name: "Mohsin",
+      URL: "https://tse4.mm.bing.net/th?id=OIP.d-ZzZ7JwBoTqyjWGMAG9mAHaFj&pid=Api&P=0&w=236&h=177",
+    },
+    {
+      name: "Hassan",
+      URL: "https://tse4.mm.bing.net/th?id=OIP.d-ZzZ7JwBoTqyjWGMAG9mAHaFj&pid=Api&P=0&w=236&h=177",
+    },
+  ]);
+
+  const swiped = (Direction, nameToDelete) => {
+    console.log("removing" + nameToDelete);
+  };
+
+  const outOfFrame = (name) => {
+    console.log(name + "left of the Screen");
+  };
   return (
-    <div>TinderCards</div>
-  )
+    <div className="tinderCards">
+      <div className="tindercards__Cardcontainer">
+        {people.map((person) => (
+          <TinderCards
+            className="swipe"
+            key={person.name}
+            preventswipe={["up", "down"]}
+            onswipe={(dir) => SwipeDown(dir, person.name)}
+            onCardLeftScreen={() => outOfFrame(character.name)}
+          ></TinderCards>
+        ))}
+
+            
+
+      </div>
+    </div>
+  );
 }
 
-export default TinderCards
+export default TinderCards;
