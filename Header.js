46fad677e856b335a743f3aa9f494e8d736d2a1f
mohsinhassan20 <mohsinbwp2018@gmail.com>
2022-03-08 00:20:41 +0500

    ,
    .

--- Header.js
+++ Header.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./Header.css";
 import PersonIcon from "@mui/icons-material/Person";
 import { IconButton } from "@material-ui/core";
+import ForumIcon from "@mui/icons-material/Forum";
 function Header() {
   return (
     <div className="header">
@@ -14,6 +15,9 @@ function Header() {
         src="https://www.vectorico.com/wp-content/uploads/2018/02/Tinder-Icon-300x300.png"
         alt=""
       />
+      <IconButton fontSize="large" className="header__icon">
+        <ForumIcon />
+      </IconButton>
     </div>
   );
 }
---
+++ TinderCards.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import "./TinderCards.css"
+function TinderCards() {
+  return (
+    <div>TinderCards</div>
+  )
+}
+
+export default TinderCards
