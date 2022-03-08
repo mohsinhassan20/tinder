fa624aeeb3a2008b07b750c982e904070d51dcaf
mohsinhassan20 <mohsinbwp2018@gmail.com>
2022-03-08 15:49:26 +0500

    ..

---
+++ SwipeButtons.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./SwipeButtons.css";
+import ReplayIcon from "@mui/icons-material/Replay";
+import CloseIcon from "@mui/icons-material/Close";
+import StarRateIcon from "@mui/icons-material/StarRate";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import FlashOnIcon from "@mui/icons-material/FlashOn";
+
+function SwipeButtons() {
+  return <div className="swipeButtons">SwipeButtons</div>;
+}
+
+export default SwipeButtons;
